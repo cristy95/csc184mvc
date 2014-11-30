74008b662aac35c6a2fe99ee26d31285d06a1016
Kring2 <cristylorainefuerzas@gmail.com>
2014-11-30 17:55:02 +0800

    added a borg singleton file

---
+++ borg_singleton.py
@@ -0,0 +1,14 @@
+class Borg(object):
+	_shared_state = {}
+
+	def __new__(cls, *args, **kwargs):
+
+		obj = super(Borg, cls).__new__(cls, *args, **kwargs)
+		obj.__dict__ = cls._shared_state
+		return obj
+
+class Child(Borg):
+	pass
+
+class Another Child(Borg):
+	_shared_state = {}
